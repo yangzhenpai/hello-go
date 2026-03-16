33f017622b1b9468f8beb8be823a43d3526e0b85
yangzhenpai <82381385@qq.com>
2026-03-16 23:46:03 +0800

    更新了 string  代码

--- string.go
+++ string.go
@@ -1,9 +1,16 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	//定义字符串
+	var name string = "小明"
+	var address string = "中国"
+	// 短变量声明
+	phone := "13800000000"
+
+	// 输出字符串
+	fmt.Println(name)
+	fmt.Println(address)
+	fmt.Println(phone)
 }
